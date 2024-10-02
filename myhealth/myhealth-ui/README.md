fc9f82787c6999f61293cfe59af875f7a72ca652
Yuan Ji <yuan.ji@jiwhiz.com>
2024-10-02 00:41:27 -0600

    Implement myhealth-ui

---
+++ myhealth/myhealth-ui/README.md
@@ -0,0 +1,16 @@
+# myhealth-ui
+> Built with [Elm Land](https://elm.land) 🌈
+
+## Local development
+
+```bash
+# Requires Node.js v18+ (https://nodejs.org)
+HOST=ui.mydoctor POET=1233 elm-land server
+```
+
+## Deploying to production
+
+Elm Land projects are most commonly deployed as static websites. 
+
+Please visit [the "Deployment" guide](https://elm.land/guide/deploying) to learn more
+about deploying your app for free using Netlify or Vercel.
---
+++ myhealth/myhealth-ui/src/interop.js
@@ -0,0 +1,72 @@
+import Keycloak from 'keycloak-js';
+
+var keycloak = new Keycloak({
+    url: 'http://auth.myhealth:8090',
+    realm: 'myhealth-demo',
+    clientId: 'myhealth-ui',
+});
+
+
+// This is called BEFORE your Elm app starts up
+// 
+// The value returned here will be passed as flags 
+// into your `Shared.init` function.
+export const flags = ({ env }) => {
+}
+
+// This is called AFTER your Elm app starts up
+//
+// Here you can work with `app.ports` to send messages
+// to your Elm application, or subscribe to incoming
+// messages from Elm
+export const onReady = ({ app, env }) => {
+    console.log("onReady");
+
+    // Initialize Keycloak
+    keycloak
+    .init({
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+            window.location.origin + '/assets/silent-check-sso.html'
+    })
+    .then(function (result) {
+        console.log("After init: " + result);
+        if (keycloak.authenticated) {
+            console.log("Authenticated. Send the access token back to Elm");
+            app.ports.onLoginSuccess.send(keycloak.token);
+            localStorage.setItem('token', keycloak.token);
+        }
+    });
+
+    if (app.ports && app.ports.login && app.ports.logout) {
+
+        app.ports.login.subscribe( () => {
+            console.log("Call login()");
+            keycloak
+            .login()
+            .then(function () {
+                console.log("After login: " + keycloak.authenticated);
+                if (keycloak.authenticated) {
+                    app.ports.onLoginSuccess.send(keycloak.token);
+                }
+            })
+            .catch(function () {
+                console.error('Failed to login Keycloak');
+            });
+        })
+    
+        app.ports.logout.subscribe( () => {
+            console.log("Call logout()");
+            keycloak
+            .logout()
+            .then(function () {
+                console.log("After logout: " + keycloak.authenticated);
+            })
+            .catch(function (err) {
+                console.error('Failed to logout Keycloak');
+                console.error(err);
+            });
+        })
+    }
+
+}
